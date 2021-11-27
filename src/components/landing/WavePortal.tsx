5d17ae7eb116516f489dea813582bd56980a9ad1
nuurcodes <nuurcodes@gmail.com>
2021-11-27 22:10:29 +0000

    feat: add message input field

--- src/components/landing/WavePortal.tsx
+++ src/components/landing/WavePortal.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import { Box } from '@mui/system'
-import { Button, CircularProgress, Typography } from '@mui/material'
+import {
+  Paper,
+  Button,
+  CircularProgress,
+  TextField,
+  Typography,
+  Stack,
+} from '@mui/material'
 import { Contract } from '@config/contracts'
 import { useContract } from '@hooks/useContract'
 import { useContractFunction, useEthers } from '@usedapp/core'
@@ -10,9 +17,17 @@ enum Transaction {
   WAVE = 'Send wave',
 }
 
+type Wave = {
+  sender: string
+  message: string
+  timestamp: string
+}
+
 export default function WavePortal() {
   const { chainId } = useEthers()
+  const [message, setMessage] = React.useState('')
   const [totalWaves, setTotalWaves] = React.useState(0)
+  const [waves, setWaves] = React.useState<Wave[]>([])
   const contract = useContract(Contract.WAVE_PORTAL_CONTRACT)
 
   const { state, send } = useContractFunction(
@@ -24,11 +39,35 @@ export default function WavePortal() {
   React.useEffect(() => {
     contract
       .getTotalWaves()
-      .then((waves) => {
-        setTotalWaves(waves.toNumber())
+      .then((total) => {
+        setTotalWaves(total.toNumber())
       })
       .catch((e) => setTotalWaves(0))
   }, [contract, chainId])
+
+  React.useEffect(() => {
+    contract
+      .getAllWaves()
+      .then((waves) => {
+        setWaves(
+          waves.map((wave) => ({
+            sender: wave.sender,
+            message: wave.message,
+            timestamp: wave.timestamp.toString(),
+          }))
+        )
+      })
+      .catch((e) => setWaves([]))
+  }, [contract, chainId])
+
+  const onSendMessage = async () => {
+    try {
+      await send(message)
+      setMessage('')
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   return (
     <Box
@@ -40,19 +79,46 @@ export default function WavePortal() {
         flexDirection: 'column',
       }}
     >
-      <Typography variant='h3'>Total waves: {totalWaves}</Typography>
-      <Button
-        size='large'
-        onClick={() => send('Hello')}
-        disabled={state.status === 'Mining'}
-        endIcon={
-          state.status === 'Mining' ? (
-            <CircularProgress size='16px' color='inherit' />
-          ) : undefined
-        }
+      <Paper
+        sx={{
+          padding: 4,
+        }}
       >
-        Wave
-      </Button>
+        <Typography variant='h4'>Total waves: {totalWaves}</Typography>
+        <Box>
+          {waves.map((wave) => (
+            <>
+              <div>{wave.sender}</div>
+              <div>{wave.message}</div>
+              <div>{wave.timestamp}</div>
+            </>
+          ))}
+        </Box>
+        <Stack direction='column' spacing={2}>
+          <TextField
+            fullWidth
+            id='outlined-multiline-flexible'
+            label='Message'
+            multiline
+            maxRows={4}
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <Button
+            fullWidth
+            size='large'
+            onClick={onSendMessage}
+            disabled={state.status === 'Mining' || !message}
+            endIcon={
+              state.status === 'Mining' ? (
+                <CircularProgress size='16px' color='inherit' />
+              ) : undefined
+            }
+          >
+            Wave
+          </Button>
+        </Stack>
+      </Paper>
     </Box>
   )
 }
