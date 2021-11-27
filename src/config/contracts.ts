891d93feaa8d3d8188adc57ba2b02fa0a695810d
nuurcodes <nuurcodes@gmail.com>
2021-11-27 22:06:41 +0000

    chore: update contracts

--- src/config/contracts.ts
+++ src/config/contracts.ts
@@ -13,13 +13,13 @@ export const config: { [key: number]: { [key in Contract]: ConfigOption } } = {
   1: {
     [Contract.WAVE_PORTAL_CONTRACT]: {
       // TODO: Update address
-      address: '0xaaefaa3DE8684eFd2a119e692CF779064bFd11fB',
+      address: '0xaFAb120D40DCFEDf490Ae890e05341d26767580C',
       abi: WavePortalAbi__factory.abi,
     },
   },
   4: {
     [Contract.WAVE_PORTAL_CONTRACT]: {
-      address: '0xaaefaa3DE8684eFd2a119e692CF779064bFd11fB',
+      address: '0xaFAb120D40DCFEDf490Ae890e05341d26767580C',
       abi: WavePortalAbi__factory.abi,
     },
   },
