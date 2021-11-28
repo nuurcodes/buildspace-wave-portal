7e07da5f3f4d1192a85b4c3267c15f48f8f84fa6
nuurcodes <nuurcodes@gmail.com>
2021-11-28 22:05:15 +0000

    chore: change supported networks

--- src/config/core.ts
+++ src/config/core.ts
@@ -1,10 +1,9 @@
 import { ChainId, Config } from '@usedapp/core'
 
 export const config: Config = {
-  readOnlyChainId: ChainId.Mainnet,
+  readOnlyChainId: ChainId.Rinkeby,
   readOnlyUrls: {
-    [ChainId.Mainnet]: process.env.NEXT_PUBLIC_RPC_MAINNET as string,
     [ChainId.Rinkeby]: process.env.NEXT_PUBLIC_RPC_RINKEBY as string,
   },
-  supportedChains: [ChainId.Mainnet, ChainId.Rinkeby],
+  supportedChains: [ChainId.Rinkeby],
 }
